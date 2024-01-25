8b8b8e1f75f65f0595d523909f1173aec92d7cdd
Gureumi <cloud9350@naver.com>
2024-01-25 10:05:31 +0900

    Initial commit

---
+++ src/main/java/com/hkit/lifetime/LifetimeApplication.java
@@ -0,0 +1,13 @@
+package com.hkit.lifetime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifetimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifetimeApplication.class, args);
+    }
+
+}
