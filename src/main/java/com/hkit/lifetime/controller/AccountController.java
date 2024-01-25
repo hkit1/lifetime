ef9488b1479d985fb8664e06f186e3c6b576f151
Gureumi <cloud9350@naver.com>
2024-01-25 13:54:44 +0900

    ??

--- src/main/java/com/hkit/lifetime/controller/AccountController.java
+++ src/main/java/com/hkit/lifetime/controller/AccountController.java
@@ -1,11 +1,11 @@
 package com.hkit.lifetime.controller;
 
 import com.hkit.lifetime.dto.AccountDto;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
-@Service
+@Controller
 public class AccountController {
     @GetMapping("login")
     public void login(@ModelAttribute AccountDto accountDto) {
