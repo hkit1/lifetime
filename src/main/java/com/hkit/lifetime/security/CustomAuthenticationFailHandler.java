32a780468ca74e55db9a4a336fd7cadc60650bf3
HKIT <dydals918@gmail.com>
2024-02-19 17:11:54 +0900

    login failed exception update

--- src/main/java/com/hkit/lifetime/security/CustomAuthenticationFailHandler.java
+++ src/main/java/com/hkit/lifetime/security/CustomAuthenticationFailHandler.java
@@ -1,42 +1,28 @@
 package com.hkit.lifetime.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.authentication.*;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 
 import java.io.IOException;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
-public class CustomAuthenticationFailHandler implements AuthenticationFailureHandler {
+public class CustomAuthenticationFailHandler extends SimpleUrlAuthenticationFailureHandler {
 
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-//        request.getRequestDispatcher("/member/login").forward(request, response);
-
-        //로그인 실패 시 401 반환
-//        response.setStatus(HttpStatus.UNAUTHORIZED.value());
-
-        String errorMessage;
-
-        if(exception instanceof BadCredentialsException) {
-            errorMessage = "아이디 또는 비밀번호가 맞지 않습니다. 다시 확인해주세요.";
-        } else if (exception instanceof InternalAuthenticationServiceException) {
-            errorMessage = "내부 시스템 문제로 로그인 요청을 처리할 수 없습니다. 관리자에게 문의하세요. ";
-        } else if (exception instanceof UsernameNotFoundException) {
-            errorMessage = "존재하지 않는 계정입니다. 회원가입 후 로그인해주세요.";
-        } else if (exception instanceof AuthenticationCredentialsNotFoundException) {
-            errorMessage = "인증 요청이 거부되었습니다. 관리자에게 문의하세요.";
-        } else {
-            errorMessage = "알 수 없는 오류로 로그인 요청을 처리할 수 없습니다. 관리자에게 문의하세요.";
-        }
-
-        request.setAttribute("errorMessage", errorMessage);
+        setDefaultFailureUrl("/member/login-error");
+        super.onAuthenticationFailure(request, response, exception);
     }
-
 }
