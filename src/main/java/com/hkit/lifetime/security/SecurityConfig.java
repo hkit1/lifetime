a1c741ffb601ea03c19664b76d4099766356385f
Gureumi <cloud9350@naver.com>
2024-02-21 14:22:15 +0900

    Fix missing login

--- src/main/java/com/hkit/lifetime/security/SecurityConfig.java
+++ src/main/java/com/hkit/lifetime/security/SecurityConfig.java
@@ -10,7 +10,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -27,7 +26,7 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests(requsts -> requsts
                         //권한 없이 들어갈 수 있는 페이지
-                        .requestMatchers("/", "/member/login", "/account/register**", "/register/complete", "/api/lecture/{id}/image").permitAll()
+                        .requestMatchers("/", "/member/login", "/member/login-error", "/account/register**", "/register/complete", "/api/lecture/{id}/image").permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN")
                         //이외의 페이지는 모두 권한 필요
                         .anyRequest().authenticated()
